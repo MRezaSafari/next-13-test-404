3db5068efb200bdc83d94048aa34efbc0b19f555
reza <reza@Reza-PC.local>
2023-02-17 17:16:40 +0330

    try reprod

--- next.config.js
+++ next.config.js
@@ -1,8 +1,57 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+const securityHeaders = [
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on',
+	},
+	{
+		key: 'Strict-Transport-Security',
+		value: 'max-age=63072000; includeSubDomains; preload',
+	},
+	{
+		key: 'X-XSS-Protection',
+		value: '1; mode=block',
+	},
+	{
+		key: 'X-XSS-Protection',
+		value: '1; mode=block',
+	},
+	{
+		key: 'platform',
+		value: 'web',
+	},
+];
 
-module.exports = nextConfig
+const moduleExports = {
+	swcMinify: true,
+	poweredByHeader: false,
+	experimental: {
+		optimisticClientCache: true,
+		webVitalsAttribution: ['CLS', 'LCP'],
+		profiling: true,
+		appDir: true,
+	},
+	async headers() {
+		return [
+			{
+				source: '/:path*',
+				headers: securityHeaders,
+			},
+		];
+	},
+	reactStrictMode: false,
+	images: {
+		formats: ['image/webp', 'image/avif'],
+		minimumCacheTTL: 86400,
+		deviceSizes: [320, 640, 750, 828, 1080, 1200, 1320, 1920, 2640],
+		imageSizes: [8, 16, 32, 48, 64, 96, 128, 256, 384],
+	},
+	compiler: {
+		removeConsole: process.env.NODE_ENV === 'production',
+	},
+
+};
+
+
+module.exports = moduleExports
+
+
